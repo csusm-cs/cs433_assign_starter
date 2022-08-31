586a2d98411324fbc7b00f4447c79576e7eea580
xiaoyu12 <xiaoyu@csusm.edu>
2022-08-31 12:03:23 -0700

    rename readyqueue files to lower case

---
+++ assign1/readyqueue.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "readyqueue.h"
+
+using namespace std;
+
+//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
+// Remember to add sufficient comments to your code
+
+
+/**
+ * @brief Constructor for the ReadyQueue class.
+ */
+ ReadyQueue::ReadyQueue()  {
+     //TODO: add your code here
+ }
+
+/**
+ * @brief Add a PCB representing a process into the ready queue.
+ *
+ * @param pcbPtr: the pointer to the PCB to be added
+ */
+void ReadyQueue::addPCB(PCB *pcbPtr) {
+    //TODO: add your code here
+    // When adding a PCB to the queue, you must change its state to READY.
+}
+
+/**
+ * @brief Remove and return the PCB with the highest priority from the queue
+ *
+ * @return PCB*: the pointer to the PCB with the highest priority
+ */
+PCB* ReadyQueue::removePCB() {
+    //TODO: add your code here
+    // When removing a PCB from the queue, you must change its state to RUNNING.
+}
+
+/**
+ * @brief Returns the number of elements in the queue.
+ *
+ * @return int: the number of PCBs in the queue
+ */
+int ReadyQueue::size() {
+    //TODO: add your code here
+}
+
+/**
+ * @brief Display the PCBs in the queue.
+ */
+void ReadyQueue::displayAll() {
+    //TODO: add your code here
+}
---
+++ assign1/readyqueue.h
@@ -0,0 +1,64 @@
+/**
+ * Assignment 1: priority queue of processes
+ * @file readyqueue.h
+ * @author ??? (TODO: your name)
+ * @brief ReadyQueue is a queue of PCB's that are in the READY state to be scheduled to run.
+ * It should be a priority queue such that the process with the highest priority can be selected next.
+ * @version 0.1
+ */
+//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
+// Remember to add sufficient comments to your code
+#pragma once
+
+#include "pcb.h"
+
+/**
+ * @brief A queue of PCB's that are in the READY state to be scheduled to run.
+ * It should be a priority queue such that the process with the highest priority can be selected next.
+ */
+class ReadyQueue {
+private:
+    // TODO: add your private member variables here
+    // choose a data structure for the ReadyQueue. No STL class is allowed.
+
+public:
+    /**
+     * @brief Construct a new ReadyQueue object
+     *
+     */
+    ReadyQueue();
+
+    /**
+     * @brief Destructor
+     */
+    ~ReadyQueue() {}
+
+	// You may add additional member functions, but don't change the definitions of the following four member functions.
+
+    /**
+     * @brief Add a PCB representing a process into the ready queue.
+     *
+     * @param pcbPtr: the pointer to the PCB to be added
+     */
+	void addPCB(PCB* pcbPtr);
+
+    /**
+     * @brief Remove and return the PCB with the highest priority from the queue
+     *
+     * @return PCB*: the pointer to the PCB with the highest priority
+     */
+	PCB* removePCB();
+
+    /**
+     * @brief Returns the number of elements in the queue.
+     *
+     * @return int: the number of PCBs in the queue
+     */
+	int size();
+
+     /**
+      * @brief Display the PCBs in the queue.
+      */
+	void displayAll();
+
+};
