6bc0f3d3e2eca9064a70c23620bd73f7295a058d
NicholasFAndrew <124222703+NicholasFAndrew@users.noreply.github.com>
2023-09-12 15:41:48 -0700

    Added names

--- assign1/pcb.h
+++ assign1/pcb.h
@@ -1,7 +1,7 @@
 /**
  * Assignment 1: priority queue of processes
  * @file pcb.h
- * @author ??? (TODO: your name)
+ * @author Nick F. Andrew & Julian Rangel
  * @brief This is the header file for the PCB class, a process control block.
  * @version 0.1
  */
