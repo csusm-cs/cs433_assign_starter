8486dc3f8a6527290e0104929089f63930175c03
NicholasFAndrew <124222703+NicholasFAndrew@users.noreply.github.com>
2023-09-12 15:42:18 -0700

    added names

--- assign1/pcbtable.cpp
+++ assign1/pcbtable.cpp
@@ -1,7 +1,7 @@
 /**
  * Assignment 1: priority queue of processes
  * @file pcbtable.h
- * @author ??? (TODO: your name)
+ * @author Nick F. Andrew & Julian Rangel
  * @brief This is the implementation file for the PCBTable class.
  * //You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
  * // Remember to add sufficient comments to your code
