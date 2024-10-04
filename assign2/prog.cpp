79107d9856576c7281e3963edcbfc48881035564
Caden Jamason <me@lutrine.dev>
2024-10-04 10:18:16 -0700

    ♻️ refactor(assign2): Improve command parsing and add process execution
    Refactor the `parse_command` function to improve the parsing of commands and arguments from the input. Also, add the functionality to execute the parsed command using `execvp`.

--- assign2/prog.cpp
+++ assign2/prog.cpp
@@ -26,12 +26,23 @@ using namespace std;
  * @brief parse out the command and arguments from the input command separated
  * by spaces
  *
- * @param command
- * @param args
- * @return int
+ * @param command The input command
+ * @param args The array of arguments parsed from the input command
+ * @return int – The number of arguments
  */
 int parse_command(char command[], char *args[]) {
   // TODO: implement this function
+  char *token = strtok(command, " \n"); // Scans command for first space
+  int i = 0;                          // Counter for the number of arguments
+
+  while (token != NULL) {      // While the token isn't NULL...
+    args[i] = token;           // Add the token to the args array
+    token = strtok(NULL, " \n"); // Scan for the next space
+    i++;                       // Increment the counter
+  }
+
+  args[i] = NULL; // Set the last argument to NULL to mark end of array
+  return i;       // Return the number of arguments
 }
 
 // TODO: Add additional functions if you need
@@ -49,6 +60,7 @@ int main(int argc, char *argv[]) {
   int should_run = 1;           /* flag to determine when to exit program */
 
   // TODO: Add additional variables for the implementation.
+  pid_t pid; // Process ID
 
   while (should_run) {
     printf("osh>");
@@ -59,12 +71,37 @@ int main(int argc, char *argv[]) {
     int num_args = parse_command(command, args);
 
     // TODO: Add your code for the implementation
+
+    // If no arguments, continue to next iteration
+    if (num_args == 0) {
+      continue;
+    }
+
+    // Compare first argument to "exit" to determine if shell should exit
+    if (strcmp(args[0], "exit") == 0) {
+      should_run = 0;
+      continue;
+    }
+
     /**
      * After reading user input, the steps are:
      * (1) fork a child process using fork()
      * (2) the child process will invoke execvp()
      * (3) parent will invoke wait() unless command included &
      */
+
+    pid = fork();
+    if (pid < 0) { // If fork fails
+      fprintf(stderr, "Fork failed\n");
+      return 1;
+    } else if (pid == 0) { // If child process
+      if (execvp(args[0], args) < 0) { // Execute the command
+        fprintf(stderr, "Execution failed\n");
+        return 1;
+      }
+    } else { // If parent process
+      wait(NULL); // Wait for child process to finish
+    }
   }
   return 0;
 }
