2c902acf75e883b815fd3538cdb9f3b73b147029
Erin Bailey <146312546+Moosecapade@users.noreply.github.com>
2024-10-23 16:05:47 -0700

    Update scheduler_rr.cpp

--- assign3/scheduler_rr.cpp
+++ assign3/scheduler_rr.cpp
@@ -5,6 +5,9 @@
  * @brief This Scheduler class implements the RoundRobin (RR) scheduling algorithm.
  * @version 0.1
  */
+//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
+// Remember to add sufficient and clear comments to your code
+//
 
 #include "scheduler_rr.h"
 #include <deque>
@@ -12,16 +15,14 @@
 /**
  * @brief Construct a new SchedulerRR object
  */
-SchedulerRR::SchedulerRR(int time_quantum = 10){
+SchedulerRR::SchedulerRR(int time_quantum){
     this->time_quantum = time_quantum;
 }
 
 /**
  * @brief Destroy the SchedulerRR object
  */
-SchedulerRR::~SchedulerRR() {
-    delete curr_proc;
-}
+SchedulerRR::~SchedulerRR() {}
 
 /**
  * @brief This function is called once before the simulation starts.
@@ -42,13 +43,13 @@ void SchedulerRR::init(std::vector<PCB>& process_list) {
  */
 void SchedulerRR::print_results() {
 
-    for(int i = 0; i < times.size(); i++){
-        int ttime = times.at(i).at(0) + times.at(i).at(1);
-        int wtime = times.at(i).at(1);
+    for(int i = 0; i < count; i++){
+        int ttime = times.at(i).at(0);
+        int wtime = times.at(i).at(0) - times.at(i).at(1);
         avg_turnaround += ttime;
         avg_wait += wtime;
 
-        std::cout << "T" << i << " turn-around time = " << ttime << ", waiting time = " << wtime << std::endl;
+        std::cout << "T" << i + 1 << " turn-around time = " << ttime << ", waiting time = " << wtime << std::endl;
     }
 
     avg_turnaround = avg_turnaround / count;
@@ -66,7 +67,7 @@ void SchedulerRR::simulate() {
         curr_proc = &proc_li.front(); // curr_proc holds the highest priority process
         proc_li.pop_front();          // remove highest priority process from list
 
-        if(time_quantum < curr_proc->burst_time){       // if burst time is longer than the quantum
+        if(unsigned(time_quantum) < curr_proc->burst_time){       // if burst time is longer than the quantum
             std::cout << "Running Process " << curr_proc->name << " for " << time_quantum << " time units" << std::endl;
             elapsed_time += time_quantum;               // only record passage of the time quantum
             times.at(curr_proc->id).at(1) += time_quantum;
@@ -77,12 +78,8 @@ void SchedulerRR::simulate() {
             elapsed_time += curr_proc->burst_time;    // if burst time is shorter, record passage of burst time
             times.at(curr_proc->id).at(1) += curr_proc->burst_time;
             curr_proc->burst_time = 0;
-            curr_proc = nullptr;
+            times.at(curr_proc->id).at(0) = elapsed_time;
         }
-
-
-
-        curr_proc = nullptr;
     }
 }
 
