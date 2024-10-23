2b9b997ee88e2d938d28200efec69c04f83eafff
Erin Bailey <146312546+Moosecapade@users.noreply.github.com>
2024-10-22 18:36:37 -0700

    Update scheduler_priority.cpp

--- assign3/scheduler_priority.cpp
+++ assign3/scheduler_priority.cpp
@@ -1,70 +1,84 @@
 /**
- * Driver (main) program for Priority RR scheduling algorithm.
- * The input file is a text file containing the process information in the following format:
- * [name] [priority] [CPU burst]
+* Assignment 3: CPU Scheduler
+ * @file scheduler_priority.cpp
+ * @author Erin Bailey(433.01), Zack Miller(433.?)
+ * @brief This Scheduler class implements the Priority scheduling algorithm.
+ * @version 0.1
  */
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <sstream>
-#include "scheduler_priority_rr.h"
 
-using namespace std;
+#include "scheduler_priority.h"
 
-int main(int argc, char *argv[]) {
-    std::cout << "CS 433 Programming assignment 3" << std::endl;
-    std::cout << "Author: Erin Bailey(433.01) and Zack Miller(433.?)" << std::endl;     // TODO: add your name
-    std::cout << "Date: 10/22/2024" << std::endl;               // TODO: add date
-    std::cout << "Course: CS433 (Operating Systems)" << std::endl;
-    std::cout << "Description : test Priority RR scheduling algorithm " << std::endl;
-    std::cout << "=================================" << std::endl;
+/**
+ * @brief Construct a new SchedulerPriority object
+ */
+SchedulerPriority::SchedulerPriority(){}
 
-    // Make sure the user has provided the input file name
-    if (argc < 3) {
-        cerr << "Usage: " << argv[0] << " <input_file> <time quantum>" << endl;
-        exit(1);
+/**
+ * @brief Destroy the SchedulerPriority object
+ */
+SchedulerPriority::~SchedulerPriority(){}
+
+/**
+ * @brief This function is called once before the simulation starts.
+ *        It is used to initialize the scheduler.
+ * @param process_list The list of processes in the simulation.
+ */
+void SchedulerPriority::init(std::vector<PCB>& process_list){
+    for(PCB A: process_list){
+        proc_li.push_back(A);
+        count++;
+        times.push_back({0,0});
     }
 
-    // Read the time quantum if provided.
-    int time_quantume = atoi(argv[2]);
+    std::sort(proc_li.begin(), proc_li.end()); // sort proc_li in ascending order by priority
+}
 
-    // Read the input file
-    ifstream input_file(argv[1]);
-    // Make sure the file is open
-    if (!input_file.is_open()) {
-        cerr << "Error: Unable to open file " << argv[1] << endl;
-        exit(1);
-    }
-    string line;
-    vector<PCB> process_list;
-    int id = 0;
-    while(getline(input_file, line)) {
-        // Split the line into tokens
-        istringstream ss(line);
-        string name;
-        // parse out the name
-        getline(ss, name, ',');
-        // parse out the priority
-        string token;
-        getline(ss, token, ',');
-        unsigned int priority = stoi(token);
-        // parse out the burst time
-        getline(ss, token, ',');
-        unsigned int burst_time = stoi(token);
+/**
+ * @brief This function is called once after the simulation ends.
+ *        It is used to print out the results of the simulation.
+ */
+void SchedulerPriority::print_results(){
 
-        // Create a PCB object and add it to the process list
-        PCB pcb(name, id, priority, burst_time);
-        id++;
-        pcb.print();
-        process_list.push_back(pcb);
+    for(int i = 0; i < count; i++){
+        int ttime = times.at(i).at(0) + times.at(i).at(1);
+        int wtime = times.at(i).at(0);
+        avg_turnaround += ttime;
+        avg_wait += wtime;
+
+        std::cout << "T" << i + 1 << " turn-around time = " << ttime << ", waiting time = " << wtime << std::endl;
     }
 
-    // Create a scheduler object
-    SchedulerPriorityRR scheduler (time_quantume);
-    // Run the scheduler
-    scheduler.init(process_list);
-    scheduler.simulate();
-    scheduler.print_results();
+    avg_turnaround = avg_turnaround / count;
+    avg_wait = avg_wait / count;
+    std::cout << "Average turn-around time = " << avg_turnaround << ", Average waiting time = " << avg_wait << std::endl;
+
+}
+
+/**
+ * @brief This function simulates the scheduling of processes in the ready queue.
+ *        It stops when all processes are finished.
+ */
+void SchedulerPriority::simulate(){
+    // at this point, proc_li was initialized, atd sorted into descending order
+    
+    while(proc_li.size() > 0){
+        curr_proc = &proc_li.front(); // curr_proc holds the highest priority process
+        
+        std::cout << "Running Process " << curr_proc->name << " for " << curr_proc->burst_time << " time units" << std::endl;
+        
+        proc_li.pop_front();          // remove highest priority process from list
+        times.at(curr_proc->id).at(0) = elapsed_time;
+        times.at(curr_proc->id).at(1) = curr_proc->burst_time;
+        elapsed_time += curr_proc->burst_time;
+    }
+
+}
+
+/**
+ * @brief overloads the < operator so that it works on PCB objects
+ *        this causes proc_li to sort in ascending order
+ */
+bool operator< (PCB A, PCB B){
+    return A.priority > B.priority;
 }
