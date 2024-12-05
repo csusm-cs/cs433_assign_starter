727a126d6a24d821071bd9facc5160db4528a5db
Erin Bailey <146312546+Moosecapade@users.noreply.github.com>
2024-12-05 11:12:00 -0800

    Update fifo_replacement.cpp

--- assign5/fifo_replacement.cpp
+++ assign5/fifo_replacement.cpp
@@ -1,20 +1,17 @@
 /**
 * Assignment 5: Page replacement algorithms
  * @file fifo_replacement.cpp
- * @author ??? (TODO: your name)
+ * @author Erin Bailey and Zach Miller
  * @brief A class implementing the FIFO page replacement algorithms
  * @version 0.1
  */
-//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
 // Remember to add sufficient and clear comments to your code
 
 #include "fifo_replacement.h"
 
-// TODO: Add your implementation here
 FIFOReplacement::FIFOReplacement(int num_pages, int num_frames)
 : Replacement(num_pages, num_frames) {}
 
-// TODO: Add your implementations for desctructor, load_page, replace_page here
 FIFOReplacement::~FIFOReplacement() {}
 
 // Access an invalid page, but free frames are available
