a63425a5f081f8a857859a1b081d664914cca993
Erin Bailey <146312546+Moosecapade@users.noreply.github.com>
2024-12-05 11:16:55 -0800

    Update lifo_replacement.h

--- assign5/lifo_replacement.h
+++ assign5/lifo_replacement.h
@@ -1,11 +1,10 @@
 /**
 * Assignment 5: Page replacement algorithms
  * @file lifo_replacement.h
- * @author ??? (TODO: your name)
+ * @author Zach Miller and Erin Bailey
  * @brief A class implementing the LIFO page replacement algorithms
  * @version 0.1
  */
-//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
 // Remember to add sufficient and clear comments to your code
 
 #pragma once
