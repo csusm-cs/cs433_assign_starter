bca4a285a1c713dee0b854d9987d723f15b02a76
Erin Bailey <146312546+Moosecapade@users.noreply.github.com>
2024-12-05 11:10:09 -0800

    Update fifo_replacement.cpp

--- assign5/fifo_replacement.cpp
+++ assign5/fifo_replacement.cpp
@@ -12,23 +12,27 @@
 
 // TODO: Add your implementation here
 FIFOReplacement::FIFOReplacement(int num_pages, int num_frames)
-: Replacement(num_pages, num_frames)
-{
-    // TODO: Add additional implementation code
-}
+: Replacement(num_pages, num_frames) {}
 
 // TODO: Add your implementations for desctructor, load_page, replace_page here
-FIFOReplacement::~FIFOReplacement() {
-    // TODO: Add necessary code here
-}
+FIFOReplacement::~FIFOReplacement() {}
 
 // Access an invalid page, but free frames are available
 void FIFOReplacement::load_page(int page_num) {
-    // TODO: Update your data structure FIFO replacement and pagetable
+    page_table[page_num].frame_num = used_frames;	// used_frames doubles as a frame number recorder here
+    page_table[page_num].valid = true;				// make the new page valid
+	used_frames++;
 }
 
 // Access an invalid page and no free frames are available
 int FIFOReplacement::replace_page(int page_num) {
-    // TODO: Update your data structure FIFO replacement and pagetable
+    for(int i = 0; i < num_pages; i++){
+        if(page_table[i].valid == true && page_table[i].frame == least_frame){ // if the page is a valid frame ref AND it's frame is least_frame
+            page_table[i].valid = false;                    // make found page invalid
+            page_table[page_num].frame = least_frame;		// make new page have the least_frame
+            page_table[page_num].valid = true;				// make new page valid
+            least_frame = (least_frame + 1) % num_frames;	// increment least_frame to the next value, resets to 0 after reaching the max num_frames
+        }
+    }
     return 0;
 }
