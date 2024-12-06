ef4f63b087b5e6b8a9177286e171c173cc8ad2d7
Erin Bailey <146312546+Moosecapade@users.noreply.github.com>
2024-12-05 19:04:15 -0800

    Update main.cpp

--- assign5/main.cpp
+++ assign5/main.cpp
@@ -20,8 +20,8 @@ int main(int argc, char *argv[]) {
     //Print basic information about the program
     std::cout << "=================================================================" << std::endl;
     std::cout << "CS 433 Programming assignment 5" << std::endl;
-    std::cout << "Author: xxxxxx and xxxxxxx" << std::endl;
-    std::cout << "Date: xx/xx/20xx" << std::endl;
+    std::cout << "Author: Zach Miller and Erin Bailey" << std::endl;
+    std::cout << "Date: 12/05/2024" << std::endl;
     std::cout << "Course: CS433 (Operating Systems)" << std::endl;
     std::cout << "Description : Program to simulate different page replacement algorithms" << std::endl;
     std::cout << "=================================================================\n" << std::endl;
